430d403cd312e4744ef19ebe0e1c190caebdf3c7
Mohamed Elgohary <mohamedelgohary883@gmail.com>
2026-07-14 08:58:55 +0300

    create service failure

--- lib/features/home/presentation/manager/home_rebo.dart
+++ lib/features/home/presentation/manager/home_rebo.dart
@@ -1,7 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRebo {
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<ServerFailure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<ServerFailure, List<BookModel>>> fetchNewestBooks();
 }

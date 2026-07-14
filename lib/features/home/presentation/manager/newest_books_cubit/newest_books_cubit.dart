a795114b37491fb6c2aa9c99237d06c2cf16d01c
Mohamed Elgohary <mohamedelgohary883@gmail.com>
2026-07-14 09:56:20 +0300

    finish newest books cubit

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repo/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit({required this.homeRepo}) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestdBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errorMessage: failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
+}
